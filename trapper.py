9d70b3926bc0b44126b4ef93b7d99b515b17f777
Simachew Mengiste <absima@gmail.com>
2023-12-30 08:07:09 +0100

    error in direction of motion fixed

--- trapper.py
+++ trapper.py
@@ -2,7 +2,6 @@ import pygame
 import sys
 import numpy as np
 
-pygame.init()
 
 # Constants
 canvas_width = 700 
@@ -11,7 +10,7 @@ canvas_height = 700
 frame_rate = 60 #frame per second
 
 trapper_count = 15
-trapped_count = 5
+trapped_count = 20
 # trap_radius = 200
 trap_radius = 0.8 * 1e-11
 trapped_speed = 2
@@ -25,7 +24,7 @@ static_color = 'red'
 mobile_color = 'yellow'
 
 q_trapped = -4.8e-10 #charge
-charge_scale = 2
+charge_scale = 2.
 q_trapper = charge_scale*q_trapped
 m_trapped = 1.67e-30 #mass in grams
 
@@ -38,31 +37,95 @@ def mtmRate(pos):
     pdots = np.zeros((len(pos),2))
     for i, pos_i in enumerate(pos):
         # against the peers
-        vec_ip = np.delete(pos, i, 0) - pos_i
+        vec_ip =  pos_i - np.delete(pos, i, 0)
         pscaler = q_trapped**2/np.linalg.norm(vec_ip, axis=1)**3
         pterm = vec_ip * np.column_stack((pscaler, pscaler))
         # against the trappers
-        vec_it = pos_trpr-pos_i
+        vec_it = pos_i - pos_trpr
         tscaler = q_trapper*q_trapped/np.linalg.norm(vec_it, axis=1)**3
         tterm = vec_it * np.column_stack((tscaler,tscaler))
     
         pdots[i] = np.sum(pterm,0)+np.sum(tterm,0)
     return pdots
     
+def transform(pos):
+    ### to screen
+    pdiag_src = scale*np.array([-1, -1])
+    pdiag_tgt = -pdiag_src
+    sdiag_src = canvas_height*np.array([0, 1])
+    sdiag_tgt = canvas_width*np.array([1, 0])
 
-def transform(pos, opt):
-    pdiag_vec = np.array([2*scale, 2*scale]) #from (-c,-c) to (c,c)
-    sdiag_vec = np.array([canvas_width, canvas_height])
 
-    sbleft = -sdiag_vec/2 # bottom left corner coordinate
-    ## assuming diag vectors in standard position:
-    if opt=="to_screen":
-        return pos*sdiag_vec/pdiag_vec  + sdiag_vec/2
-    elif opt=="to_micro":
-        return (pos-sdiag_vec/2)*pdiag_vec/sdiag_vec # first term: bottom left coord
-        # return (pos-sdiag_vec/2)*sdiag_vec/pdiag_vec
-    else:
-        return None
+    pdiag_vec = pdiag_tgt - pdiag_src
+    sdiag_vec = sdiag_tgt - sdiag_src#np.array([canvas_width, canvas_height])
+
+    tpos = sdiag_src + (pos - pdiag_src) * sdiag_vec/pdiag_vec
+
+    return tpos
+                    
+
+pygame.init()
+
+# Canvas
+screen = pygame.display.set_mode((canvas_width, canvas_height))
+pygame.display.set_caption("Plasmatrap Simulation")
+
+# # Creating the trapping (static) particles in an equidistant circular order
+#     # pygame.draw.circle(screen, static_color, [posx,posy], tr_particle_size) ### draw
+ang_trpr = np.arange(trapper_count)*2*np.pi/trapper_count
+pos_trpr = trap_radius * np.column_stack((np.cos(ang_trpr), np.sin(ang_trpr)))
+pos_trapper = transform(pos_trpr) 
+    
+
+# initial random position of the trapped sprites 
+pos_real = np.zeros((trapped_count, 2))
+radiuss = np.random.rand(trapped_count)*trap_radius
+thetaas = np.random.rand(trapped_count)*2*np.pi
+pos_real = np.column_stack((radiuss*np.cos(thetaas), radiuss*np.sin(thetaas)))
+
+mtm_trapped = np.zeros((trapped_count, 2))
+while True:
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    
+    ## simple Euler method
+    pdots = mtmRate(pos_real)
+    mtm_trapped += dt*pdots
+    pos_real += dt*mtm_trapped/m_trapped
+    
+    ### magnify to screen canvas
+    pos_trapped = transform(pos_real)
+    
+    print ("positions")
+    # print(pos_trapped)
+    print()
+   
+    screen.fill(screen_color)
+    i=0
+    for trapper in pos_trapper:
+        if i==0:
+            static_color = 'white'
+        elif i==1:
+            static_color = 'blue'
+        else:
+            static_color = 'red'
+        i+=1
+        pygame.draw.circle(screen, static_color, trapper.tolist(), tr_particle_size) ### draw
+    
+    for particle in pos_trapped:
+        print(particle)
+        pygame.draw.circle(screen, mobile_color, particle.tolist(), td_particle_size)
+    
+    pygame.display.flip()
+    pygame.time.Clock().tick(frame_rate)
+
+# def transform(pos):
+#     pdiag_vec = np.array([2*scale, 2*scale]) #from (-c,-c) to (c,c)
+#     sdiag_vec = np.array([canvas_width, canvas_height])
+#     return pos*sdiag_vec/pdiag_vec  + sdiag_vec/2
+    
     
 # def transform(vec, opt):
 #     pdiag_vec = np.array([2*scale, 2*scale]) #from (-c,-c) to (c,c)
@@ -85,82 +148,3 @@ def transform(pos, opt):
 #         return None
 #
 #     return tpos
-                    
-
-# Canvas
-screen = pygame.display.set_mode((canvas_width, canvas_height))
-pygame.display.set_caption("Plasmatrap Simulation")
-
-# # Creating the trapping (static) particles in an equidistant circular order
-# pos_trpr = np.zeros((trapper_count, 2)) #infinite mass
-# for i in range(trapper_count):
-#     # posx = canvas_width / 2 + trap_radius * np.cos(2 * np.pi * i / trapper_count)
-#     # posy = canvas_height / 2 + trap_radius * np.sin(2 * np.pi * i / trapper_count)
-#     posx = trap_radius * np.cos(2 * np.pi * i / trapper_count)
-#     posy = trap_radius * np.sin(2 * np.pi * i / trapper_count)
-#     pos_trpr[i] = [posx, posy]
-#
-#     # pygame.draw.circle(screen, static_color, [posx,posy], tr_particle_size) ### draw
-ang_trpr = np.arange(trapper_count)*2*np.pi/trapper_count
-pos_trpr = trap_radius * np.column_stack((np.cos(ang_trpr), np.sin(ang_trpr)))
-pos_trapper = transform(pos_trpr, "to_screen") 
-    
-
-# initial random position of the trapped sprites 
-pos_real = np.zeros((trapped_count, 2))
-radiuss = np.random.rand(trapped_count)*trap_radius
-thetaas = np.random.rand(trapped_count)*2*np.pi
-pos_real = np.column_stack((radiuss*np.cos(thetaas), radiuss*np.sin(thetaas)))
-
-mtm_trapped = np.zeros((trapped_count, 2))
-while True:
-    for e in pygame.event.get():
-        if e.type == pygame.QUIT:
-            pygame.quit()
-            sys.exit()
-    
-    ## simple Euler method
-    pdots = mtmRate(pos_real)
-    mtm_trapped += dt*pdots
-    pos_real += dt*mtm_trapped/m_trapped
-    
-    ### magnify to screen canvas
-    # pos_trapped = canvas_width*pos_real/scale/2
-    pos_trapped = transform(pos_real, "to_screen")
-    
-    print ("positions", pos_trapped)
-   
-    screen.fill(screen_color)
-    for trapper in pos_trapper:
-        pygame.draw.circle(screen, static_color, trapper.tolist(), tr_particle_size) ### draw
-    
-    for particle in pos_trapped:
-        print(particle)
-        pygame.draw.circle(screen, mobile_color, particle.tolist(), td_particle_size)
-    
-    pygame.display.flip()
-    pygame.time.Clock().tick(frame_rate)
-
-
-
-# def mtmRate(pos):
-#     # estimate of pdot, rate of momentum
-#     pdots = np.zeros((len(pos),2))
-#     for i, pos_i in enumerate(pos):
-#         # with peers
-#         vec_ip = np.delete(pos, i, 0) - pos_i
-#         pscaler = q_trapped**2/np.sqrt(np.sum(vec_ip**2, 1))**3
-#         pscaler = np.column_stack((pscaler, pscaler))
-#         wpeers = np.sum(pscaler * vec_ip, 0)
-#         # with trappers
-#         tpos_real = 2*scale/canvas_width*pos_trapper
-#         vec_it = tpos_real-pos_i
-#         tscaler = q_trapper*q_trapped/np.sqrt(np.sum(vec_it**2, 1))**3
-#         tscaler = np.column_stack((tscaler, tscaler))
-#         wtrapr = np.sum(tscaler * vec_it,0)
-#
-#         pdots[i] = wpeers+wtrapr
-#     return pdots
-
-
- 
